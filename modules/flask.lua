d7e4521960baf3984b39eb9eb3335235a79c6375
Zachary Schneider <schneider@boundary.com>
2015-06-30 13:58:16 -0500

    fix flask bin path

--- modules/flask.lua
+++ modules/flask.lua
@@ -95,7 +95,7 @@ function Flask:initialize(filename)
   end)
 
   -- process status
-  self.process = PidStatus:new('/opt/boundary-platform-flask/bin/flask')
+  self.process = PidStatus:new('/opt/flask/bin/flask')
   self.process:on("status", function(status)
     self:filter(status)
   end)
